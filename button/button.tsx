ac1b58c73d1681906b650cb407ec1c45a80329e4
Lazar Mitic <Lazar__93@hotmail.com>
2020-05-23 14:11:06 +0200

    Add button component

---
+++ button/button.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+
+import Typography, { TypographyVariant } from './../typography/typography';
+
+import './button.scss';
+import ThemeContext from '../theme/theme-context';
+
+interface ButtonProps {
+	label: string;
+	onClick?: () => void;
+	fullWidth?: boolean;
+	disabled?: boolean;
+	width?: number;
+}
+
+class Button extends React.Component<ButtonProps, {}> {
+	constructor(props: ButtonProps) {
+		super(props);
+	}
+
+	public render = (): JSX.Element => {
+		let buttonWidth = '';
+		if (this.props.fullWidth) {
+			buttonWidth = '100%';
+		}
+		if (this.props.width) {
+			buttonWidth = `${this.props.width}px`;
+		}
+
+		return (
+			<ThemeContext.Consumer>
+				{(theme) => {
+					return (
+						<div style={{
+							width: buttonWidth,
+							backgroundColor: this.props.disabled ? theme.primary2 : theme.primary
+						}} className='button-container' onClick={this.onClick}>
+							<Typography variant={TypographyVariant.HEADING_MEDIUM} color='#FFFFFF'>
+								{this.props.label}
+							</Typography>
+						</div>
+					);
+				}}
+			</ThemeContext.Consumer>
+		);
+	}
+
+	private onClick = (): void => {
+		if (this.props.disabled) {
+			return;
+		}
+		this.props.onClick();
+	}
+}
+export default Button;
