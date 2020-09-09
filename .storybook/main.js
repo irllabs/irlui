11839b24e80968c6785822ab90bf7c002ebbcdc3
Angelina Kolosenok <angelina.kolosenok@gmail.com>
2020-09-09 13:19:13 +0300

    adding theme context to the library

--- .storybook/main.js
+++ .storybook/main.js
@@ -21,20 +21,6 @@ module.exports = {
       include: path.resolve(__dirname, '../'),
     });
 
-    // config.module.rules.push({
-        
-    //         test: /\.svg$/,
-    //         use: [
-    //           {
-    //             loader: 'svg-url-loader',
-    //             options: {
-    //               limit: 10000,
-    //             jsx: true // true outputs JSX tags
-    //             },
-    //           },
-    //         ],
-    // })
-
     // Return the altered config
     return config;
   },
